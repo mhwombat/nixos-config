f09eaff21debfa2c3af6992a3083216c302b4bbd
Amy de Buitléir <amy@nualeargais.ie>
2022-05-11 15:44:30 +0100

    +/- various pkgs

--- packages.nix
+++ packages.nix
@@ -3,9 +3,9 @@
 with pkgs;
 {
 
-  nixpkgs.config.permittedInsecurePackages = [
-    "nodejs-10.24.1"
-  ];
+  # nixpkgs.config.permittedInsecurePackages = [
+  #   "nodejs-10.24.1"
+  # ];
 
   nixpkgs.overlays = [
     (import ./overlays/default.nix)
@@ -15,15 +15,17 @@ with pkgs;
   environment.systemPackages = [
     # My custom packages
     pkgs.jot
-    pkgs.pandoc-select-code
-    pkgs.pandoc-logic-proof
+    # pkgs.pandoc-linear-table
+    # pkgs.pandoc-logic-proof
+    # pkgs.pandoc-columns
+    # pkgs.pandoc-select-code
     # Standard packages
     alacritty # X and wayland
     aspell
     aspellDicts.en
     aspellDicts.en-computers
     aspellDicts.en-science
-    autotiling
+    autotiling # tiling for sway
     bash
     bemenu
     bibclean
@@ -32,13 +34,13 @@ with pkgs;
     bind # for dig
     binutils-unwrapped
     # busybox overwrites realpath!
-    cabal2nix
+    # cabal2nix
     cabal-install
     cage # wayland, provides Xwayland to qtile
     chromium # X-only? See ozone?
-    clipman
+    # clipman
     csvkit
-    curl
+    # curl
     dialog
     diff-pdf
     diffpdf
@@ -46,53 +48,68 @@ with pkgs;
     dmidecode # system hardware info
     dos2unix
     # (dwl.override { conf = /home/amy/.config/dwl/config.h; enable-xwayland = true; })
-    dzen2
+    # dzen2
     (import ./emacs.nix { inherit pkgs; }) # X-only
     evince # X and wayland
     fbreader # X only. Binary is called "FBReader"
     firefox # X and wayland
     font-awesome
-    fzf
-    fzy
+    # fzf
+    # fzy
     gcc
     ghc
-    ghostscript # for pdf2dsc
+    # ghostscript # for pdf2dsc
     gitAndTools.gitFull
     git-crypt
+    # (gitit.override { pluginSupport = true; })
     git-lfs
+    globalprotect-openconnect
     gimp # Current version is X-only. Newer versions support wayland
     # globalprotect-openconnect
     gmp # GNU multiple precision arithmetic library
-    gnome3.adwaita-icon-theme # to help meld?
+    # gnome3.adwaita-icon-theme # to help meld?
     gnome3.dconf-editor # X and wayland
     gnome3.eog # X and wayland
     gnome3.gnome-disk-utility # X and wayland
     gnome3.gucharmap # X and wayland
-    gnome3.gnome-themes-extra # to fix "adwaita" bug
+    # gnome3.gnome-themes-extra # to fix "adwaita" bug
     gnumake
     gnutls
     gparted # X and wayland?
     graphviz
     hardinfo
     haskell-language-server
+    haskellPackages.brittany # for language server
     haskellPackages.citeproc
     # haskellPackages.doi # broken as of 21.05
+    haskellPackages.floskell # for language server
+    haskellPackages.fourmolu # for language server
     haskellPackages.ghcide # for language server
     haskellPackages.haskell-language-server
     haskellPackages.hasktags
     haskellPackages.hlint
+    haskellPackages.hls-brittany-plugin # for language server
     haskellPackages.hls-call-hierarchy-plugin # for language server
     haskellPackages.hls-class-plugin # for language server
     haskellPackages.hls-eval-plugin # for language server
+    haskellPackages.hls-floskell-plugin # for language server
+    haskellPackages.hls-fourmolu-plugin # for language server
     haskellPackages.hls-haddock-comments-plugin # for language server
     haskellPackages.hls-hlint-plugin # for language server
+    haskellPackages.hls-ormolu-plugin # for language server
     haskellPackages.hls-pragmas-plugin # for language server
+    haskellPackages.hls-stylish-haskell-plugin # for language server
     haskellPackages.hls-tactics-plugin # for language server
     # haskellPackages.ihaskell
+    haskellPackages.ormolu
     haskellPackages.pandoc
+    # haskellPackages.pandoc-citeproc # Deprecated in favour of haskellPackages.citeproc
     haskellPackages.pandoc-crossref
+    # haskellPackages.pandoc-filter-graphviz # broken as of 21.11?
     haskellPackages.pandoc-types
     # haskellPackages.pandoc-include # broken as of 21.05
+    # haskellPackages.stylish-cabal # for language server
+    haskellPackages.stylish-haskell # for language server
     hplip
     hplipWithPlugin
     imagemagick
@@ -100,20 +117,23 @@ with pkgs;
     inetutils # provides telnet
     inkscape # X and wayland
     ispell
+    kdiff3
     killall
     kitty # X and wayland
     lftp
     libertine
     libreoffice # X and wayland
+    librsvg # for including SVG in PDF
     libsForQt5.okular
     libsForQt5.spectacle # Buggy on wayland # replaced ksnapshot
     libsForQt512.kcolorpicker
     libwacom
     libxml2 # for xmllint
+    liquidprompt
     lm_sensors
     lsof
     lshw
-    lxqt.qterminal # X and wayland
+    # lxqt.qterminal # X and wayland
     meld # X and wayland
     memtester
     mkpasswd
@@ -122,11 +142,12 @@ with pkgs;
     nix-index # provides nix-locate
     nix-prefetch-git
     # nixos-generators
-    nodePackages.sloc
+    # nodePackages.sloc
     offlineimap
     onedrive
     openssl
     # opera # X and wayland, invoke with opera --enable-features=UseOzonePlatform --ozone-platform=wayland %U
+    ormolu
     p7zip
     pandoc
     pdfgrep
@@ -135,14 +156,18 @@ with pkgs;
     poppler_utils # for pdfinfo
     # pup # for parsing HTML
     python
-    python3Env
+    # python3Env
     qpdfview # X and wayland
     qtile # X and wayland
+    # retext # markdown editor
     signal-desktop # X-only
-    slurp
+    slurp # wayland only, region selector
     spaceship-prompt
+    starship
+    # stylish-cabal
+    stylish-haskell
     # sway enabled in wayland.nix
-    sxiv # X-only
+    # sxiv # X-only
     # tabula # extract tables from PDF files
     texlive.combined.scheme-full
     tree
@@ -152,13 +177,14 @@ with pkgs;
     vistafonts # True-type fonts from MS Windows
     vlc # X-only until I set QT environment
     wacomtablet
-    waybar
-    wayland-scanner
-    wayland-utils
-    wayland-protocols
+    waybar # wayland only
+    # wayland-scanner
+    # wayland-utils
+    # wayland-protocols
     wev # X and wayland, installed with sway by default
     wget
     wl-clipboard
+    wl-color-picker # wayland only
     wlr-randr
     wpa_supplicant
     # xf86_input_wacom
