976e4794b84bd15b2faac57859c3e47300dca9ac
Amy de Buitléir <amy@nualeargais.ie>
2022-09-04 19:17:58 +0100

    reduce global installs

--- overlays/default.nix
+++ overlays/default.nix
@@ -1,9 +1,8 @@
 final: prev: {
-  maths-web = final.callPackage ./maths-web {};
   amy-jot = final.haskellPackages.callPackage ./jot {};
   pandoc-linear-table = final.haskellPackages.callPackage ./pandoc-linear-table {};
   pandoc-logic-proof = final.haskellPackages.callPackage ./pandoc-logic-proof {};
   pandoc-columns = final.haskellPackages.callPackage ./pandoc-columns {};
   pandoc-select-code = final.haskellPackages.callPackage ./pandoc-select-code {};
-  pandoc-server = final.haskellPackages.callPackage ./pandoc-server {};
+  # pandoc-server = final.haskellPackages.callPackage ./pandoc-server {};
 }
