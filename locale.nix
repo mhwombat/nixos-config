6f0cf1672dd8a45510adbe13eb7b4f0f2108a543
Amy de Buitléir <amy@nualeargais.ie>
2023-05-27 21:34:29 +0100

    refactor

--- locale.nix
+++ locale.nix
@@ -1,13 +1,23 @@
 { config, ... }:
 
 {
-  # Select internationalisation properties.
-  console = {
-    font = "Lat2-Terminus16";
-    keyMap = "uk";
-  };
+  # Configure console keymap
+  console.keyMap = "ie";
+
   i18n = {
     defaultLocale = "en_IE.UTF-8";
+  };
+
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "en_IE.UTF-8";
+    LC_IDENTIFICATION = "en_IE.UTF-8";
+    LC_MEASUREMENT = "en_IE.UTF-8";
+    LC_MONETARY = "en_IE.UTF-8";
+    LC_NAME = "en_IE.UTF-8";
+    LC_NUMERIC = "en_IE.UTF-8";
+    LC_PAPER = "en_IE.UTF-8";
+    LC_TELEPHONE = "en_IE.UTF-8";
+    LC_TIME = "en_IE.UTF-8";
   };
 
   # Set your time zone.
