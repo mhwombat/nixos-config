26a73c54e25ed7bbf7a59fc42aab53b8104aae78
Amy de Buitléir <amy@nualeargais.ie>
2023-05-30 14:29:53 +0100

    autoconfigure printers

--- printers.nix
+++ printers.nix
@@ -2,5 +2,11 @@
 
 {
   # Enable CUPS to print documents.
+  # For a printer that is IPP everywhere capable, this is all you need.
+  # Turn the printer on, and CUPS will find it automatically.
+  # No need to add the printer manually using the CUPS web UI.
   services.printing.enable = true;
+  services.avahi.enable = true;
+  services.avahi.nssmdns = true;
+  services.avahi.openFirewall = true; # Optional, for Wifi
 }
