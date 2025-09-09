69341ec74ae17c25aa1b6a6620e3b773e29c578c
Amy de Buitléir <amy@nualeargais.ie>
2025-09-09 18:28:02 +0100

    bug workaround

--- overlays/default.nix
+++ overlays/default.nix
@@ -5,4 +5,19 @@ final: prev: {
   pandoc-logic-proof = final.haskellPackages.callPackage ./pandoc-logic-proof {};
   pandoc-columns = final.haskellPackages.callPackage ./pandoc-columns {};
   pandoc-select-code = final.haskellPackages.callPackage ./pandoc-select-code {};
+
+  pythonPackagesExtensions = prev.pythonPackagesExtensions ++ [
+    (python-final: python-prev: {
+    # Workaround for bug #437058
+      i3ipc = python-prev.i3ipc.overridePythonAttrs (oldAttrs: {
+        doCheck = false;
+        checkPhase = ''
+          echo "Skipping pytest in Nix build"
+        '';
+        installCheckPhase = ''
+          echo "Skipping install checks in Nix build"
+        '';
+      });
+    })
+  ];
 }
