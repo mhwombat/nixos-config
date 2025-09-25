3c83b3e38fa1a9731e67b287949896c5cb657752
Amy de Buitléir <amy@nualeargais.ie>
2025-09-25 19:42:44 +0100

    sage

--- base.nix
+++ base.nix
@@ -11,39 +11,8 @@
 #  nix.package = pkgs.nixUnstable;
   nix.extraOptions = "experimental-features = nix-command flakes";
 
-  # Support for configurable keyboard
-#  services.udev.packages = with pkgs; [ vial via ];
-
   # These variables end up in /etc/set-environment.
   # They may not take effect until you log out and back in again.
   environment.variables = import ./environment-variables.nix;
-
-  # Enable the OpenSSH daemon.
-  # services.sshd.enable = true;
-
-  # Define a user account. Don't forget to set a password with ‘passwd’.
-  users.users.amy = {
-    isNormalUser = true;
-    home = "/home/amy";
-    description = "Amy de Buitléir";
-    extraGroups = [ "wheel" "networkmanager" "vboxsf" "docker" "audio" "dialout" ];
-    uid = 1000;
-    shell = pkgs.bashInteractive;
-  };
-
-#  # Automatic upgrades
-#  system.autoUpgrade = {
-#    enable = true;
-#    allowReboot = false; # Do nixos-rebuild switch --upgrade instead
-#    channel = "https://channels.nixos.org/nixos-unstable";
-#  };
-
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "24.05"; # Did you read the comment?
 
 }
