ece4516874a02f823c3a92f5d3dc768f81cfdcb3
Amy de Buitléir <amy@nualeargais.ie>
2025-09-25 20:00:49 +0100

    sage

--- base.nix
+++ base.nix
@@ -8,11 +8,27 @@
   # nixpkgs.config.allowBroken = true;
 
   # Enable Nix flakes
-#  nix.package = pkgs.nixUnstable;
   nix.extraOptions = "experimental-features = nix-command flakes";
 
   # These variables end up in /etc/set-environment.
   # They may not take effect until you log out and back in again.
   environment.variables = import ./environment-variables.nix;
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.amy = {
+    isNormalUser = true;
+    home = "/home/amy";
+    description = "Amy de Buitléir";
+    extraGroups = [ "wheel" "networkmanager" "vboxsf" "docker" "audio" "dialout" ];
+    uid = 1000;
+    shell = pkgs.bashInteractive;
+  };
+
+#  # Automatic upgrades
+#  system.autoUpgrade = {
+#    enable = true;
+#    allowReboot = false; # Do nixos-rebuild switch --upgrade instead
+#    channel = "https://channels.nixos.org/nixos-unstable";
+#  };
 
 }
--- configuration.nix
+++ configuration.nix
@@ -23,29 +23,8 @@ in
 #      ./scheduling/recollindex.nix
     ];
 
-  # Define a user account. Don't forget to set a password with ‘passwd’.
-  users.users.amy = {
-    isNormalUser = true;
-    description = "Amy de Buitléir";
-    home = "/home/amy";
-    extraGroups = [ "networkmanager" "wheel" ];
-    packages = with pkgs; [
-    #  thunderbird
-    ];
-  };
-
   # Install firefox.
   programs.firefox.enable = true;
-
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
-
-  # List services that you want to enable:
 
   # Enable the OpenSSH daemon.
   services.openssh.enable = true;
@@ -58,13 +37,5 @@ in
   # networking.firewall.allowedUDPPorts = [ ... ];
   # Or disable the firewall altogether.
   # networking.firewall.enable = false;
-
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "25.05"; # Did you read the comment?
 
 }
