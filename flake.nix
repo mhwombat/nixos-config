d98319047a70d4bf64931cbbb7bcef14e96e945d
Amy de Buitléir <amy@nualeargais.ie>
2025-09-26 13:27:32 +0100

    refactor

--- flake.nix
+++ flake.nix
@@ -8,6 +8,7 @@
   outputs = { self, nixpkgs, ... }@inputs: {
 
     nixosConfigurations = {
+
       wombat11k = nixpkgs.lib.nixosSystem {
         specialArgs = {
           system = "x86_64-linux";
@@ -30,8 +31,30 @@
             ./scheduling/email-reminders.nix
 	    ./scheduling/recollindex.nix
 	  ];
-      };
-    };
+      }; # wombat11k
+
+      sage = nixpkgs.lib.nixosSystem {
+        specialArgs = {
+          system = "x86_64-linux";
+          hostname = "sage";
+          inherit inputs;
+        };
+        modules =
+	  [
+            (./. + "/sage-hardware-configuration.nix")
+            (./. + "/sage.nix")
+            ./base.nix
+            ./sound.nix
+            ./printers.nix
+            ./email.nix
+            ./locale.nix
+#            ./wayland.nix
+            ./packages.nix
+	    ./shannagh-wifi.nix
+	  ];
+      }; # sage
+
+    }; # nixosConfigurations
 
   };
 
