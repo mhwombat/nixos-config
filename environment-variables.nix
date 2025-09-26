f705a5c8991c741e79c4d506e35d8e444123fba1
Amy de Buitléir <amy@nualeargais.ie>
2025-09-26 16:04:22 +0100

    expanded

--- environment-variables.nix
+++ environment-variables.nix
@@ -7,7 +7,7 @@ rec {
   XDG_CACHE_HOME = "$HOME/.cache";
   XDG_DATA_HOME = "$HOME/.local/share";
   XDG_STATE_HOME = "$HOME/.local/state";
-  
+
   # To see all keyboard layouts: localectl list-x11-keymap-layouts
   XKB_DEFAULT_LAYOUT = "ie,gr";
   # To see all keyboard variants: localectl list-x11-keymap-variants
@@ -15,13 +15,14 @@ rec {
   # To see all keyboard options: localectl list-x11-keymap-options
   # Configure Alt+CapsLock to switch between keyboard layouts.
   XKB_DEFAULT_OPTIONS = "compose:rctrl-altgr,grp:alt_caps_toggle";
-  
+
   # XAUTHORITY = "$XDG_RUNTIME_DIR/Xauthority"; # xorg-auth
   # XCOMPOSEFILE = "$XDG_CONFIG_HOME/X11/xcompose"; # libX11
   # XCOMPOSECACHE = "$XDG_CACHE_HOME/X11/xcompose"; # libX11
   STACK_ROOT = "${XDG_DATA_HOME}/stack"; # Stack
   LESSHISTFILE = "${XDG_CACHE_HOME}/history"; # less
   ZDOTDIR = "$XDG_CONFIG_HOME/zsh"; # zsh
-  MOZ_ENABLE_WAYLAND = "1"; # enable wayland support in Firfox
+  MOZ_ENABLE_WAYLAND = "1"; # enable wayland support in Firefox
+  NIXOS_OZONE_WL = "1"; # enable wayland support in chromium and electron-based qapps
 }
 
