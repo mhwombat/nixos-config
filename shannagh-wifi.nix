dfd5330a5e3941c933a299379a6c3b41cc766298
Amy de Buitléir <amy@nualeargais.ie>
2025-09-26 16:00:43 +0100

    wifi interface

--- shannagh-wifi.nix
+++ shannagh-wifi.nix
@@ -8,7 +8,7 @@
     wireless = {
       enable = true;  # Enables wireless support via wpa_supplicant.
       userControlled.enable = true; # enable use of wpa_cli (or wpa_gui if you install wpa_supplicant_gui)
-      interfaces = [ "wlo2" ];
+      interfaces = [ "wlp0s20f3" ];
       networks = {
         # pskRaw is generated with the command: wpa_passphrase SSID password
 #        "vodafone4806D2".pskRaw = "e7f79c45987c0ca510a19b3600ae34f4df8046db3d7e789f789ea6b2f7aa518c";
